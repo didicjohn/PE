efed27ae65300b934688c462370d310c66aad494
John, Didi <Didicjohn@gmail.com>
2021-01-27 23:42:51 +0530

    PRomotion on same product

--- PromotionEngineUnitTest/PromotionEngineTests.cs
+++ PromotionEngineUnitTest/PromotionEngineTests.cs
@@ -7,23 +7,33 @@ namespace PromotionEngineUnitTest
 {
     public class PromotionEngineTests
     {
-        readonly Cart _sut;
+        readonly Cart _cart;//system under test
         public PromotionEngineTests()
         {
-            _sut = new Cart();
+            _cart = new Cart();
         }
 
         [Theory]
         [MemberData(nameof(TestData))]
-        public void TestCheckout(decimal amount, Dictionary<string, int> _cartItems, Dictionary<string, decimal> _products)
+        public void TestCheckout(decimal amount, Dictionary<string, int> _cartItems, Dictionary<string, decimal> _products, List<Promotion> _promotions)
         {
-            _sut.CartItemsQty = _cartItems;
-            _sut.ProductValues = _products;
-            Assert.Equal(amount, _sut.Checkout());
+            _cart.CartItemsQty = _cartItems;
+            _cart.ProductValues = _products;
+            _cart.Promotions = _promotions;
+            Assert.Equal(amount, _cart.Checkout());
         }
         public static IEnumerable<object[]> TestData()
         {
-            yield return new object[] {100,new Dictionary<string, int>() {{"A",1 },{"B",1},{"C",1}}, new Dictionary<string, decimal>() { { "A", 50 }, { "B", 30 }, { "C", 20 } } };
+            var productList = new Dictionary<string, decimal>() { { "A", 50 }, { "B", 30 }, { "C", 20 }, { "D", 15 } };
+            var promotions = new List<Promotion>(){new PromotionOnSameType(){
+                 OfferOnSameItems = new List<OfferOnSameProduct>() {
+                     new OfferOnSameProduct() { ItemName = "A", Count = 3, OfferValue = 130 },
+                     new OfferOnSameProduct(){ ItemName="B",Count = 2,OfferValue =45 } }} };
+            yield return new object[] {100,
+            new Dictionary<string, int>() {{"A",1 },{"B",1},{"C",1}},
+            productList ,promotions};
+            yield return new object[] { 370, new Dictionary<string, int>() { { "A", 5 }, { "B", 5 }, { "C", 1 } }, productList, promotions };
+            // yield return new object[] {280,new Dictionary<string, int>() {{"A",3 },{"B",5},{"C",1}, { "D", 1 } }, new Dictionary<string, decimal>() { { "A", 50 }, { "B", 30 }, { "C", 20 }, { "D", 15 } } };
         }
     }
 }
